61a87622c078c751467fecdac4cc3504edc8931c
CJ Avilla <cjavilla@gmail.com>
2013-07-08 09:51:12 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FatPenguin::Application.config.secret_token = '3b9c6e4addc109fed03bf1b0eab7973e538560ed964b25c2d00ba856a24e8ea106437d6570a3fd1b5c7e18515b444658bceee1a19f59198dc9db2ccf5894ee3f'
